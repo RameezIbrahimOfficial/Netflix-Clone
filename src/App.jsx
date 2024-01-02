19485feb89bc60a7d62bfe98ef42bd4119259d12
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2024-01-02 15:55:56 +0530

    Added ComedyMovies, HorrorMovies, RomanceMovies, Documentaries Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Banner from './Components/Banner/Banner'
 import NavBar from './Components/NavBar/NavBar'
 import RowPost from './Components/RowPost/RowPost'
-import { originals, actions } from './urls'
+import { originals, actions, ComedyMovies, HorrorMovies, RomanceMovies, Documentaries } from './urls'
 
 function App() {
   return (
@@ -11,6 +11,10 @@ function App() {
       <Banner />
       <RowPost url={originals} title="Nelfix Originals"/>
       <RowPost url={actions} title="Action" isSmall/>
+      <RowPost url={ComedyMovies} title="Comedy" isSmall/>
+      <RowPost url={HorrorMovies} title="Horror" isSmall/>
+      <RowPost url={RomanceMovies} title="Romance" isSmall/>
+      <RowPost url={Documentaries} title="Documentary" isSmall/>
     </>
   )
 }
